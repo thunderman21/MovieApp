457792f405feb660f9f318cc36aee9c7c5fce382
Michael Lang'at <langat92@gmail.com>
2020-06-09 16:48:22 +0300

    Successfully added movieObj in MovieDetails activity

--- app/src/main/java/com/michaellangat/movieapp/MovieDetails.java
+++ app/src/main/java/com/michaellangat/movieapp/MovieDetails.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MovieDetails extends AppCompatActivity {
 
@@ -14,6 +19,20 @@ public class MovieDetails extends AppCompatActivity {
 
         //get movie details from main activity
         Intent movieIntent = getIntent();
-        String movieDetailsString = movieIntent.getStringExtra("movie");
+        String movieDetailsString = movieIntent.getStringExtra("movie_details");
+
+        //create movie JSONObj
+        try {
+            JSONObject movieObj = new JSONObject(movieDetailsString);
+            Log.d("Movie_Details",movieObj.optString("movie"));
+            populateView(movieObj);
+
+        } catch(JSONException e){
+            Log.d("MovieDetailsActivity", "Casting movie string to Json", e);
+        }
     }
+     protected void populateView(JSONObject movie) throws JSONException {
+         TextView movieTitle = findViewById(R.id.movie_details_title);
+         movieTitle.setText(movie.getString("title"));
+     }
 }
